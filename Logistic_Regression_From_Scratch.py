73518b3b7d55894f0663d9b4cb54c4789835cc30
Akim <akim.tsvigun@mainsgroup.ru>
2019-06-19 19:46:10 +0300

    Changed log regression

--- Logistic_Regression_From_Scratch.py
+++ Logistic_Regression_From_Scratch.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 class LogisticRegression:
-    def __init__(self, intercept=True, l1=0, l2=0, weights=None, threshold=0.5):
+    def __init__(self, intercept=True, l1=0, l2=0,
+                 weights=None, threshold=0.5):
         self.intercept = intercept
         self.l1 = l1
         self.l2 = l2
@@ -20,26 +21,44 @@ class LogisticRegression:
                  (1 - y).T @ np.log(self.__sigmoid(-X@w)))
     
     def __gradient_loss(self, w, X, y):
-        return (X.T@(self.__sigmoid(X@w) - y[:, None]))
+        return X.T@(self.__sigmoid(X@w) - y[:, None])
     
-    def fit(self, X, y, learning_rate=0.01, num_epoch=10000, verbose=False):
+    def fit(self, X, y, learning_rate=0.01,
+            num_epoch=10000, verbose=False):
+        
+        if not (isinstance(X, np.ndarray) and isinstance(
+            y, np.ndarray)):
+            X, y = np.array(X), np.array(y)
+        if len(X.shape) != 2:
+            X = X.reshape(1, -1)
+                
         if self.intercept:
             X = LogisticRegression.__add_intercept(X)
-        # Initializing weights
-        self.weights = np.zeros((X.shape[1], 1)) if isinstance(self.weights, type(None)) else self.weights
+            
+        self.weights = np.zeros((X.shape[1], 1)) if isinstance(
+            self.weights, type(None)) else self.weights
         
         for epoch in range(1, num_epoch + 1):
-            self.weights -= learning_rate * self.__gradient_loss(self.weights, X, y)
+            self.weights -= learning_rate * self.__gradient_loss(
+                self.weights, X, y)
             if verbose and epoch % 1e+4 == 0:
-                print(f'Epoch: {epoch}; loss: {self.__loss(self.weights, X, y)}; weights: {self.weights}.')
+                print(f'Epoch: {epoch}; loss: \
+                      {self.__loss(self.weights, X, y)}; \
+                      weights:{self.weights}.')
                 
     def predict_proba(self, X):
+                
+        if not isinstance(X, np.ndarray):
+            X = np.array(X)
+        if len(X.shape) != 2:
+            X = X.reshape(1, -1)
+                
         if self.intercept:
             X = LogisticRegression.__add_intercept(X)
-        return self.__sigmoid(X@self.weights)
+        return np.ravel(self.__sigmoid(X@self.weights))
     
     def predict(self, X):
-        return self.predict_proba(X) >= self.threshold
+        return 1*(self.predict_proba(X) >= self.threshold)
     
     def get_weights(self):
         return np.ravel(self.weights)
